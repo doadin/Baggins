93c5c36b89ed3cb822f0350340810385c2224bd1
Thomas Hellström <rel@xed.se>
2020-05-06 15:10:13 +0200

    [cleanup] src/filters/ItemName.lua * Refactor out filter 'ItemName' from Baggins-Filtering.lua * Consistent use of upvalue * Generalize addon name handling * Tag some potentially dead code [Issue #24] * Remove anonymous functions from :AddCustomRule() for readability * General cleaning up

---
+++ src/filters/ItemName.lua
@@ -0,0 +1,67 @@
+--[[ ==========================================================================
+
+ItemName.lua
+
+========================================================================== ]]--
+
+local _G = _G
+
+local AddOnName, _ = ...
+local AddOn = _G[AddOnName]
+
+-- LUA Functions
+
+--- WoW API
+local GetContainerItemLink = _G.GetContainerItemLink
+local GetItemInfo = _G.GetItemInfo
+
+-- Libs
+local L = LibStub("AceLocale-3.0"):GetLocale(AddOnName)
+
+-- Test for match
+local function Matches(bag, slot, rule)
+
+    -- Empty rule?
+    if not rule.match then 
+        return false
+    end
+
+    -- Name matches?
+    local link = GetContainerItemLink(bag, slot)
+    if link then
+        local itemname = GetItemInfo(link)
+        if itemname and 
+           itemname:lower():match(rule.match:lower()) then
+            return true
+        end
+    end
+
+    return false
+
+end
+
+-- TODO: [#24] https://github.com/doadin/AddOn/issues/24
+-- Return name for specific rule
+local function GetName(rule)
+    return L["Name: "] .. (rule.match or "")
+end
+
+-- Register filter
+AddOn:AddCustomRule(
+    "ItemName", 
+    {
+        DisplayName = L["Item Name"],
+        Description = L["Filter by Name or partial name"],
+        GetName = GetName, -- TODO: [#24] https://github.com/doadin/Baggins/issues/24
+        Matches = Matches,
+        Ace3Options = 
+        {
+            match = 
+            {
+                name = L["String to Match"],
+                type = "input",
+            },
+        },
+    }
+)
+
