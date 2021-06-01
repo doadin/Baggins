92b964cea9f45bcc61b1a12da3c66675dbba4fc1
tbkizle <tbkizle@gmail.com>
2021-06-01 09:55:27 -0400

    Add Filter/Rule for Anima Items

---
+++ src/filters/Anima.lua
@@ -0,0 +1,38 @@
+--[[ ==========================================================================
+
+Anima.lua
+
+========================================================================== ]]--
+
+local _G = _G
+
+local AddOnName, _ = ...
+local AddOn = _G[AddOnName]
+
+-- Libs
+local L = LibStub("AceLocale-3.0"):GetLocale(AddOnName)
+
+local C_Item = _G.C_Item
+local IsAnimaItemByID = C_Item.IsAnimaItemByID
+local GetContainerItemLink = _G.GetContainerItemLink
+
+local function Matches(bag, slot, _)
+    local link = GetContainerItemLink(bag, slot)
+    if link and IsAnimaItemByID(link) then
+        return true
+    end
+end
+
+-- Clean rule
+local function CleanRule(rule)
+
+    rule.bagid=0
+
+end
+
+AddOn:AddCustomRule("Anima", {
+    DisplayName = "Anima",
+    Description = "Matches all Anima items.",
+    Matches = Matches,
+    CleanRule = CleanRule
+})
