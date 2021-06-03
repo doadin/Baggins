f56d060c05159cd01dcebdb923250082b857d3d2
tbkizle <tbkizle@gmail.com>
2021-06-03 11:43:19 -0400

    [src\filters\Conduit] Add Item Filter for Conduits

---
+++ src/filters/Conduit.lua
@@ -0,0 +1,38 @@
+--[[ ==========================================================================
+
+Conduit.lua
+
+========================================================================== ]]--
+
+local _G = _G
+
+local AddOnName, _ = ...
+local AddOn = _G[AddOnName]
+
+-- Libs
+--local LibStub = _G.LibStub
+--local L = LibStub("AceLocale-3.0"):GetLocale(AddOnName)
+
+local IsItemConduitByItemInfo = _G.C_Soulbinds.IsItemConduitByItemInfo
+local GetContainerItemLink = _G.GetContainerItemLink
+
+local function Matches(bag, slot, _)
+    local link = GetContainerItemLink(bag, slot)
+    if link and IsItemConduitByItemInfo(link) then
+        return true
+    end
+end
+
+-- Clean rule
+local function CleanRule(rule)
+
+    rule.bagid=0
+
+end
+
+AddOn:AddCustomRule("Conduit", {
+    DisplayName = "Conduit",
+    Description = "Matches all Conduit items.",
+    Matches = Matches,
+    CleanRule = CleanRule
+})
