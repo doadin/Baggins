3a4b3bdc9f69a0f0669a9f3e08f155d167a8aabb
tbkizle <tbkizle@gmail.com>
2021-06-27 23:57:41 -0400

    [src\filters\Toy] Fix Match

--- src/filters/Toy.lua
+++ src/filters/Toy.lua
@@ -10,17 +10,31 @@ local AddOnName, _ = ...
 local AddOn = _G[AddOnName]
 
 -- Libs
---local LibStub = _G.LibStub
---local L = LibStub("AceLocale-3.0"):GetLocale(AddOnName)
+local LibStub = _G.LibStub
+local LG = LibStub("LibGratuity-3.0")
 
 local GetContainerItemID = _G.GetContainerItemID
 
 local function Matches(bag, slot, _)
-    local itemId = GetContainerItemID(bag, slot)
-    local itemLink = C_ToyBox.GetToyLink(itemId)
-    if type(itemLink) == "string" then
+    --local itemId = GetContainerItemID(bag, slot)
+    --local itemLink = C_ToyBox.GetToyLink(itemId)
+    --if type(itemLink) == "string" then
+    --    return true
+    --end
+
+    -- Is item in bags or in bank bags?
+    if bag == -1 then
+        LG:SetInventoryItem("player", BankButtonIDToInvSlotID(slot))
+    else
+        LG:SetBagItem(bag,slot)
+    end
+
+    -- Text found in tooltip?
+    if LG:Find("Toy",3,3) then
         return true
     end
+
+    return false
 end
 
 -- Clean rule
