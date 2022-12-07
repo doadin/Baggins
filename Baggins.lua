389a5ebd2c2b752d5aba568e9b2b06373dec5759
tbkizle <tbkizle@gmail.com>
2022-12-07 16:38:44 -0500

    [Baggins][Retail] Fix Missing New C_Container UseContainerItem

--- Baggins.lua
+++ Baggins.lua
@@ -43,6 +43,8 @@ local C_ItemUpgrade = _G.C_ItemUpgrade
 local C_Item, ItemLocation, InCombatLockdown, IsModifiedClick, GetDetailedItemLevelInfo, GetContainerItemID, InRepairMode, KeyRingButtonIDToInvSlotID, C_PetJournal, C_NewItems, PlaySound =
       _G.C_Item, _G.ItemLocation, _G.InCombatLockdown, _G.IsModifiedClick, _G.GetDetailedItemLevelInfo, _G.C_Container and _G.C_Container.GetContainerItemID or _G.GetContainerItemID, _G.InRepairMode, _G.KeyRingButtonIDToInvSlotID, _G.C_PetJournal, _G.C_NewItems, _G.PlaySound
 
+local UseContainerItem = _G.C_Container and _G.C_Container.UseContainerItem or _G.UseContainerItem
+
 local WOW_PROJECT_ID = _G.WOW_PROJECT_ID
 local WOW_PROJECT_CLASSIC = _G.WOW_PROJECT_CLASSIC
 local WOW_PROJECT_BURNING_CRUSADE_CLASSIC = _G.WOW_PROJECT_BURNING_CRUSADE_CLASSIC
