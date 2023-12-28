0dc04625cf608ac25e2b527cc3892ec6fb6d0af2
tbkizle <tbkizle@gmail.com>
2023-12-28 16:59:04 -0500

    [src\filters\ScrapAddon]Update Scrap Check

--- src/filters/ScrapAddon.lua
+++ src/filters/ScrapAddon.lua
@@ -10,6 +10,8 @@ local AddOnName, _ = ...
 local AddOn = _G[AddOnName]
 
 -- WoW API
+local IsAddOnLoadable = _G.C_AddOns and _G.C_AddOns.IsAddOnLoadable or _G.IsAddOnLoadable
+local IsAddOnLoaded = _G.C_AddOns and _G.C_AddOns.IsAddOnLoaded or _G.IsAddOnLoaded
 local GetContainerItemLink = _G.C_Container and _G.C_Container.GetContainerItemLink or _G.GetContainerItemLink
 local GetContainerItemID = _G.C_Container and _G.C_Container.GetContainerItemID or _G.GetContainerItemID
 local BACKPACK_CONTAINER = _G.BACKPACK_CONTAINER
@@ -22,8 +24,11 @@ local REAGENTBANK_CONTAINER = _G.REAGENTBANK_CONTAINER
 local LibStub = _G.LibStub
 local L = LibStub("AceLocale-3.0"):GetLocale(AddOnName)
 
+if not (IsAddOnLoaded("Scrap") or IsAddOnLoadable("Scrap")) then
+    return
+end
+
 local Scrap = _G.Scrap
-if not Scrap then return end
 
 local function Matches(bag,slot,_)
     if not Scrap then return end
