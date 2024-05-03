cf6139a50abcdb84ac7cead422b49db3f9df0ac5
tbkizle <tbkizle@gmail.com>
2024-05-03 15:08:06 -0400

    Fix ItemType for Cata

--- src/filters/ItemType.lua
+++ src/filters/ItemType.lua
@@ -21,7 +21,7 @@ local GetContainerItemID = _G.C_Container and _G.C_Container.GetContainerItemID 
 local GetItemInfoInstant = _G.GetItemInfoInstant
 
 local GetAuctionItemSubClasses
-if AddOn:IsClassicWow() or AddOn:IsTBCWow() or AddOn:IsWrathWow() then
+if AddOn:IsClassicWow() or AddOn:IsTBCWow() or AddOn:IsWrathWow() or AddOn:IsCataWow() then
     GetAuctionItemSubClasses = _G.GetAuctionItemSubClasses
 end
 
@@ -106,6 +106,30 @@ end
 
 if AddOn:IsWrathWow() then
     -- scanned Sun Sep 11 04:42:32 2022 - patch 3.4.0
+    ItemTypes = {
+        [0]="Consumable",
+        [1]="Container",
+        [2]="Weapon",
+        [3]="Gem",
+        [4]="Armor",
+        [5]="Reagent",
+        [6]="Projectile",
+        [7]="Trade Goods",
+        [8]="Generic(OBSOLETE)",
+        [9]="Recipe",
+        [10]="Money(OBSOLETE)",
+        [11]="Quiver",
+        [12]="Quest",
+        [13]="Key",
+        [14]="Permanent(OBSOLETE)",
+        [15]="Miscellaneous",
+        [16]="Glyph",
+        [18]="WoW Token"
+    }
+end
+
+if AddOn:IsCataWow() then
+    -- scanned Fri May 3 15:20:54 2024 - patch 4.4.0
     ItemTypes = {
         [0]="Consumable",
         [1]="Container",
@@ -221,7 +245,7 @@ AddOn:AddCustomRule("ItemType",
                         local tmp = {}
                         tmp.ALL = _G.ALL
                         if rule.itype and ItemTypes[rule.itype] then
-                            if AddOn:IsClassicWow() or AddOn:IsTBCWow() or AddOn:IsWrathWow() then
+                            if AddOn:IsClassicWow() or AddOn:IsTBCWow() or AddOn:IsWrathWow() or AddOn:IsCataWow() then
                                 for _,k in pairs({GetAuctionItemSubClasses(rule.itype)}) do
                                     tmp[tostring(k)] = GetItemSubClassInfo(rule.itype, k) or "UNKNOWN"
                                 end
