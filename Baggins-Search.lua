d6e04f88307ebddd5fc97bf7229cfca0e64d89a7
tbkizle <tbkizle@gmail.com>
2024-06-30 14:16:03 -0400

    [Baggins-Search] Big Update

--- Baggins-Search.lua
+++ Baggins-Search.lua
@@ -35,16 +35,8 @@ if NumberofAddons >= 1 then
     end
 end
 
-local _G = _G
-
-local AddOnName, _ = ...
-local AddOn = _G[AddOnName] --luacheck: ignore 211
-
---local _, addonTable = ... -- addonName, addonTable
---local L = addonTable.L
-
 local Baggins = _G.Baggins
-local GetItemInfo = _G.C_Item.GetItemInfo
+local GetItemInfo = _G.C_Item and _G.C_Item.GetItemInfo or _G.GetItemInfo
 local GetDetailedItemLevelInfo = _G.C_Item.GetDetailedItemLevelInfo
 local strlen = _G.strlen
 local strfind = _G.strfind
@@ -122,71 +114,61 @@ function BagginsSearch:Search(search) --luacheck: ignore 212
         for _, section in ipairs(bag.sections) do --sectionid, section
             for _, button in ipairs(section.items) do --buttonid, button
                 if button:IsVisible() then
-                    local link = GetContainerItemLink(button:GetParent():GetID(), button:GetID())
-                    if link then
-                        --itemName, itemLink, itemQuality, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount,itemEquipLoc, itemTexture, sellPrice, classID, subclassID, bindType, expacID, setID, isCraftingReagent
-                        if Baggins:IsRetailWow() then
-                            itemName, _, itemQuality, itemLevel, _, itemType, itemSubType, _,itemEquipLoc, _, _, _, _, bindType, expacID, setID = GetItemInfo(link)
-                            effectiveILvl, isPreview, baseILvl = GetDetailedItemLevelInfo(link)
-                            GearILevel = effectiveILvl or baseILvl or itemLevel or 0
-                        else
-                            itemName, _, itemQuality, itemLevel, _, itemType, itemSubType, _,itemEquipLoc, _, _, _, _, bindType, expacID = GetItemInfo(link)
-                        end
-                        if not itemName then
-                            -- hack hack hack
-                            itemName = string.match(link, "|h%[(.*)%]") or ""
-                            -- TODO: should figure out what type of thing this is so we can populate these:
-                            --itemType = ""
-                            --itemSubType = ""
-                            --itemEquipLoc = ""
-                        end
-                        --if bindType == nil then
-                        --    bindType = ""
-                        --end
-                        --if expacID == nil then
-                        --    expacID = ""
-                        --end
-                        --if setID == nil then
-                        --    setID = ""
-                        --end
-                        if Baggins:IsRetailWow() then
-                            if strlen(search) == 0 then
-                                button:UnlockHighlight()
-                                button:SetAlpha(1)
-                            elseif itemName and strfind(itemName:lower(), search:lower()) or
-                            itemType and strfind(itemType:lower(), search:lower()) or
-                            itemSubType and strfind(itemSubType:lower(), search:lower()) or
-                            itemEquipLoc and strfind(itemEquipLoc:lower(), search:lower()) or
-                            bindType and strfind(itemBindTypes[bindType]:lower(), search:lower()) or
-                            bindType and strfind(itemBindTypesAB[bindType]:lower(), search:lower()) or
-                            expacID and itemExpansion[expacID] and strfind(itemExpansion[expacID]:lower(), search:lower()) or
-                            expacID and itemExpansionAB[expacID] and strfind(itemExpansionAB[expacID]:lower(), search:lower()) or
-                            itemQuality and strfind(itemQualityT[itemQuality]:lower(), search:lower()) or
-                            GearILevel and strfind(GearILevel, search) or
-                            setID and strfind(setID, search) then
-                                button:LockHighlight()
-                                button:SetAlpha(1)
+                    if strlen(search) == 0 or search == "" then
+                        button:UnlockHighlight()
+                        button:SetAlpha(tonumber(Baggins.db.profile.unmatchedAlpha) or 0.2)
+                    else
+                        local link = GetContainerItemLink(button:GetParent():GetID(), button:GetID())
+                        if link then
+                            --itemName, itemLink, itemQuality, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount,itemEquipLoc, itemTexture, sellPrice, classID, subclassID, bindType, expacID, setID, isCraftingReagent
+                            if Baggins:IsRetailWow() then
+                                itemName, _, itemQuality, itemLevel, _, itemType, itemSubType, _,itemEquipLoc, _, _, _, _, bindType, expacID, setID = GetItemInfo(link)
+                                effectiveILvl, isPreview, baseILvl = GetDetailedItemLevelInfo(link)
+                                GearILevel = effectiveILvl or baseILvl or itemLevel or 0
                             else
-                                button:UnlockHighlight()
-                                button:SetAlpha(tonumber(Baggins.db.profile.unmatchedAlpha) or 0.2)
+                                itemName, _, itemQuality, itemLevel, _, itemType, itemSubType, _,itemEquipLoc, _, _, _, _, bindType, expacID = GetItemInfo(link)
                             end
-                        else
-                            if strlen(search) == 0 then
-                                button:UnlockHighlight()
-                                button:SetAlpha(1)
-                            elseif itemName and strfind(itemName:lower(), search:lower()) or
-                            itemType and strfind(itemType:lower(), search:lower()) or
-                            itemSubType and strfind(itemSubType:lower(), search:lower()) or
-                            itemEquipLoc and strfind(itemEquipLoc:lower(), search:lower()) or
-                            bindType and strfind(itemBindTypes[bindType]:lower(), search:lower()) or
-                            bindType and strfind(itemBindTypesAB[bindType]:lower(), search:lower()) or
-                            itemQuality and strfind(itemQualityT[itemQuality]:lower(), search:lower()) or
-                            itemLevel and strfind(itemLevel, search) then
-                                button:LockHighlight()
-                                button:SetAlpha(1)
+                            if not itemName then
+                                -- hack hack hack
+                                itemName = string.match(link, "|h%[(.*)%]") or ""
+                                -- TODO: should figure out what type of thing this is so we can populate these:
+                                --itemType = ""
+                                --itemSubType = ""
+                                --itemEquipLoc = ""
+                            end
+                            if Baggins:IsRetailWow() then
+                                if itemName and strfind(itemName:lower(), search:lower()) or
+                                itemType and strfind(itemType:lower(), search:lower()) or
+                                itemSubType and strfind(itemSubType:lower(), search:lower()) or
+                                itemEquipLoc and strfind(itemEquipLoc:lower(), search:lower()) or
+                                bindType and strfind(itemBindTypes[bindType]:lower(), search:lower()) or
+                                bindType and strfind(itemBindTypesAB[bindType]:lower(), search:lower()) or
+                                expacID and itemExpansion[expacID] and strfind(itemExpansion[expacID]:lower(), search:lower()) or
+                                expacID and itemExpansionAB[expacID] and strfind(itemExpansionAB[expacID]:lower(), search:lower()) or
+                                itemQuality and strfind(itemQualityT[itemQuality]:lower(), search:lower()) or
+                                GearILevel and strfind(GearILevel, search) or
+                                setID and strfind(setID, search) then
+                                    button:LockHighlight()
+                                    button:SetAlpha(1)
+                                else
+                                    button:UnlockHighlight()
+                                    button:SetAlpha(tonumber(Baggins.db.profile.unmatchedAlpha) or 0.2)
+                                end
                             else
-                                button:UnlockHighlight()
-                                button:SetAlpha(tonumber(Baggins.db.profile.unmatchedAlpha) or 0.2)
+                                if itemName and strfind(itemName:lower(), search:lower()) or
+                                itemType and strfind(itemType:lower(), search:lower()) or
+                                itemSubType and strfind(itemSubType:lower(), search:lower()) or
+                                itemEquipLoc and strfind(itemEquipLoc:lower(), search:lower()) or
+                                bindType and strfind(itemBindTypes[bindType]:lower(), search:lower()) or
+                                bindType and strfind(itemBindTypesAB[bindType]:lower(), search:lower()) or
+                                itemQuality and strfind(itemQualityT[itemQuality]:lower(), search:lower()) or
+                                itemLevel and strfind(itemLevel, search) then
+                                    button:LockHighlight()
+                                    button:SetAlpha(1)
+                                else
+                                    button:UnlockHighlight()
+                                    button:SetAlpha(tonumber(Baggins.db.profile.unmatchedAlpha) or 0.2)
+                                end
                             end
                         end
                     end
@@ -196,79 +178,81 @@ function BagginsSearch:Search(search) --luacheck: ignore 212
     end
 end
 
+local BagginsSearch_Label
+local BagginsSearch_EditBox
 local function BagginsSearch_CreateEditBox()
     -- Create Baggins Search EditBox
     local LSM = LibStub:GetLibrary("LibSharedMedia-3.0", true) --luacheck:ignore 113
-    local editBox = CreateFrame('EditBox', 'BagginsSearch_EditBox', UIParent, BackdropTemplateMixin and "BackdropTemplate") --luacheck: ignore 113
-    editBox:SetWidth(100)
-    editBox:SetHeight(24)
+    BagginsSearch_EditBox = CreateFrame('EditBox', nil, UIParent, BackdropTemplateMixin and "BackdropTemplate") --luacheck: ignore 113
+    BagginsSearch_EditBox:SetWidth(100)
+    BagginsSearch_EditBox:SetHeight(24)
     --editBox:SetScale(Baggins.db.profile.scale)
-    editBox:SetFrameStrata("HIGH")
+    BagginsSearch_EditBox:SetFrameStrata("HIGH")
 
-    editBox:SetFontObject(ChatFontNormal)
-    editBox:SetFont(LSM and LSM:Fetch("font", Baggins.db.profile.Font) or _G.STANDARD_TEXT_FONT,Baggins.db.profile.FontSize or 10, "")
-    editBox:SetTextInsets(8, 8, 0, 0)
-    editBox:SetAutoFocus(false)
+    BagginsSearch_EditBox:SetFontObject(ChatFontNormal)
+    BagginsSearch_EditBox:SetFont(LSM and LSM:Fetch("font", Baggins.db.profile.Font) or _G.STANDARD_TEXT_FONT,Baggins.db.profile.FontSize or 10, "")
+    BagginsSearch_EditBox:SetTextInsets(8, 8, 0, 0)
+    BagginsSearch_EditBox:SetAutoFocus(false)
 
-    editBox:SetBackdrop({edgeFile = "Interface/Tooltips/UI-Tooltip-Border", edgeSize = 16, insets = {left = 2, right = 2, top = 2, bottom = 2}})
-    editBox:SetBackdropBorderColor(0.6, 0.6, 0.6)
+    BagginsSearch_EditBox:SetBackdrop({edgeFile = "Interface/Tooltips/UI-Tooltip-Border", edgeSize = 16, insets = {left = 2, right = 2, top = 2, bottom = 2}})
+    BagginsSearch_EditBox:SetBackdropBorderColor(0.6, 0.6, 0.6)
 
-    local background = editBox:CreateTexture(nil, "BACKGROUND")
+    local background = BagginsSearch_EditBox:CreateTexture(nil, "BACKGROUND")
     background:SetTexture("Interface/ChatFrame/ChatFrameBackground")
     background:SetPoint("TOPLEFT", 4, -4)
     background:SetPoint("BOTTOMRIGHT", -4, 4)
     background:SetGradient("VERTICAL", CreateColor(0, 0, 0, 0.9), CreateColor(0.2, 0.2, 0.2, 0.9))
 
-    editBox:SetScript("OnHide", function(self)
+    BagginsSearch_EditBox:SetScript("OnHide", function(self)
             self:SetText("")
-            _G.BagginsSearch_Label:Show()
+            BagginsSearch_Label:Show()
         end)
-    editBox:SetScript("OnEnterPressed", function(self) self:ClearFocus() end)
-    editBox:SetScript("OnEscapePressed", function(self)
+    BagginsSearch_EditBox:SetScript("OnEnterPressed", function(self) self:ClearFocus() end)
+    BagginsSearch_EditBox:SetScript("OnEscapePressed", function(self)
             self:SetText("")
             self:ClearFocus()
-            _G.BagginsSearch_Label:Show()
+            BagginsSearch_Label:Show()
         end)
-    editBox:SetScript("OnEditFocusGained", function(self)
+    BagginsSearch_EditBox:SetScript("OnEditFocusGained", function(self)
             if IsControlKeyDown() then
                 self:SetText("")
                 self:ClearFocus()
-                _G.BagginsSearch_Label:Show()
+                BagginsSearch_Label:Show()
             else
-                _G.BagginsSearch_Label:Hide()
+                BagginsSearch_Label:Hide()
                 self:HighlightText()
             end
         end)
-    editBox:SetScript("OnTextChanged", function(self)
+    BagginsSearch_EditBox:SetScript("OnTextChanged", function(self)
             BagginsSearch:Search(self:GetText())
         end)
-    editBox:SetScript("OnEnter", function(self)
+    BagginsSearch_EditBox:SetScript("OnEnter", function(self)
             GameTooltip_SetDefaultAnchor(GameTooltip, self)
             GameTooltip:SetText("Baggins Search")
             --GameTooltip:AddLine("|c00FFFFFFv" .. BagginsSearch.version .. "|r")
             GameTooltip:Show()
         end)
-    editBox:SetScript("OnLeave", function() GameTooltip:Hide() end)
+    BagginsSearch_EditBox:SetScript("OnLeave", function() GameTooltip:Hide() end)
 
-    local label = editBox:CreateFontString("BagginsSearch_Label", "OVERLAY", "GameFontHighlight")
-    label:SetAlpha(0.2)
-    label:SetFont(LSM and LSM:Fetch("font", Baggins.db.profile.Font) or _G.STANDARD_TEXT_FONT,Baggins.db.profile.FontSize or 10, "")
-    label:SetText("Search")
-    label:SetPoint("TOPLEFT", 8, 0)
-    label:SetPoint("BOTTOMLEFT", -8, 0)
-    label:Show()
+    BagginsSearch_Label = BagginsSearch_EditBox:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
+    BagginsSearch_Label:SetAlpha(0.2)
+    BagginsSearch_Label:SetFont(LSM and LSM:Fetch("font", Baggins.db.profile.Font) or _G.STANDARD_TEXT_FONT,Baggins.db.profile.FontSize or 10, "")
+    BagginsSearch_Label:SetText("Search")
+    BagginsSearch_Label:SetPoint("TOPLEFT", 8, 0)
+    BagginsSearch_Label:SetPoint("BOTTOMLEFT", -8, 0)
+    BagginsSearch_Label:Show()
 
 end
 
 function BagginsSearch:UpdateEditBoxPosition() --luacheck: ignore 212
     if Baggins.db.profile.enableSearch then
-        if not _G.BagginsSearch_EditBox then
+        if not BagginsSearch_EditBox then
             BagginsSearch_CreateEditBox()
         end
     end
     if not Baggins.db.profile.enableSearch then
-        if _G.BagginsSearch_EditBox then
-            _G.BagginsSearch_EditBox:Hide()
+        if BagginsSearch_EditBox then
+            BagginsSearch_EditBox:Hide()
             return
         end
     end
@@ -287,29 +271,29 @@ function BagginsSearch:UpdateEditBoxPosition() --luacheck: ignore 212
         lastBag = 1
       end
     end
-    if _G.BagginsSearch_EditBox and lastBag then
-        _G.BagginsSearch_EditBox:ClearAllPoints()
-        _G.BagginsSearch_EditBox:SetPoint("BOTTOMRIGHT", "BagginsBag"..lastBag, "TOPRIGHT", 0, 0)
-        _G.BagginsSearch_EditBox:SetWidth(getglobal("BagginsBag"..lastBag):GetWidth())
-        _G.BagginsSearch_EditBox:Show()
+    if BagginsSearch_EditBox and lastBag then
+        BagginsSearch_EditBox:ClearAllPoints()
+        BagginsSearch_EditBox:SetPoint("BOTTOMRIGHT", "BagginsBag"..lastBag, "TOPRIGHT", 0, 0)
+        BagginsSearch_EditBox:SetWidth(getglobal("BagginsBag"..lastBag):GetWidth())
+        BagginsSearch_EditBox:Show()
     else
-        if _G.BagginsSearch_EditBox then
-            _G.BagginsSearch_EditBox:Hide()
+        if BagginsSearch_EditBox then
+            BagginsSearch_EditBox:Hide()
         end
     end
 end
 
 Baggins:RegisterSignal("Baggins_UpdateBagScale",function() --luacheck: ignore 212
-    if _G.BagginsSearch_EditBox then
-        _G.BagginsSearch_EditBox:SetScale(Baggins.db.profile.scale)
+    if BagginsSearch_EditBox then
+        BagginsSearch_EditBox:SetScale(Baggins.db.profile.scale)
     end
 end, Baggins)
 
 
 Baggins:RegisterSignal("Baggins_RefreshBags",function() --luacheck: ignore 212
     BagginsSearch:UpdateEditBoxPosition()
-    if _G.BagginsSearch_EditBox then
-        BagginsSearch:Search(_G.BagginsSearch_EditBox:GetText())
+    if BagginsSearch_EditBox then
+        BagginsSearch:Search(BagginsSearch_EditBox:GetText())
     end
 end, Baggins)
 
@@ -329,14 +313,14 @@ Baggins:RegisterSignal("Baggins_AllBagsClosed",function() --luacheck: ignore 212
         lastBag = 1
       end
     end
-    if _G.BagginsSearch_EditBox and lastBag then
-        _G.BagginsSearch_EditBox:ClearAllPoints()
-        _G.BagginsSearch_EditBox:SetPoint("BOTTOMRIGHT", "BagginsBag"..lastBag, "TOPRIGHT", 0, 0)
-        _G.BagginsSearch_EditBox:SetWidth(getglobal("BagginsBag"..lastBag):GetWidth())
-        _G.BagginsSearch_EditBox:Show()
+    if BagginsSearch_EditBox and lastBag then
+        BagginsSearch_EditBox:ClearAllPoints()
+        BagginsSearch_EditBox:SetPoint("BOTTOMRIGHT", "BagginsBag"..lastBag, "TOPRIGHT", 0, 0)
+        BagginsSearch_EditBox:SetWidth(getglobal("BagginsBag"..lastBag):GetWidth())
+        BagginsSearch_EditBox:Show()
     else
-        if _G.BagginsSearch_EditBox then
-            _G.BagginsSearch_EditBox:Hide()
+        if BagginsSearch_EditBox then
+            BagginsSearch_EditBox:Hide()
         end
     end
 end, Baggins)
@@ -347,7 +331,7 @@ f:SetScript("OnEvent", function()
     if Baggins.db.profile.enableSearch then
         BagginsSearch_CreateEditBox()
         BagginsSearch:UpdateEditBoxPosition()
-        _G.BagginsSearch_EditBox:SetScale(Baggins.db.profile.scale)
+        BagginsSearch_EditBox:SetScale(Baggins.db.profile.scale)
     end
     Baggins.OnMenuRequest.args.General.args.BagginsSearchEnable = {
         name = "Enable Search",
@@ -371,7 +355,7 @@ f:SetScript("OnEvent", function()
         get = function() return Baggins.db.profile.unmatchedAlpha end,
         set = function(_, value)
             Baggins.db.profile.unmatchedAlpha = value
-            BagginsSearch:Search(_G.BagginsSearch_EditBox:GetText())
+            BagginsSearch:Search(BagginsSearch_EditBox:GetText())
         end
     }
 end)
