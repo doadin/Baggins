7c4244983a7a390adc0f9895ef86e4fe4f8c960f
Josh Sterling <josh@sterling.digital>
2024-11-02 15:44:25 -0700

    Fixed error preventing context menu from opening

--- Baggins.lua
+++ Baggins.lua
@@ -4379,11 +4379,7 @@ local function EasyMenu_Initialize(frame, level, menuList)
 end
 
 function Baggins:EasyMenu(menuList, menuFrame, anchor, x, y, displayMode, autoHideDelay)
-    if Baggins:IsRetailWow() then
-        if displayMode=='MENU' then menuFrame.displayMode = displayMode end
-        UIDropDownMenu_Initialize(menuFrame, EasyMenu_Initialize, displayMode, nil, menuList)
-        ToggleDropDownMenu(1, nil, menuFrame, anchor, x, y, menuList, nil, autoHideDelay)
-    else
-        EasyMenu(menuList, menuFrame, anchor, x, y, displayMode, autoHideDelay)
-    end
+    if displayMode=='MENU' then menuFrame.displayMode = displayMode end
+    UIDropDownMenu_Initialize(menuFrame, EasyMenu_Initialize, displayMode, nil, menuList)
+    ToggleDropDownMenu(1, nil, menuFrame, anchor, x, y, menuList, nil, autoHideDelay)
 end
